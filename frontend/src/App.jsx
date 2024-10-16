22d3084da054c4897aea59a3e724f8044b1493f8
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-10-15 22:05:28 -0300

    Criação do projeto frontend e alguns testes no react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Navbar } from "./components/navbar/Navbar"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+export function Navbar() {
+    return (
+        <nav>
+            <div>
+                <h1>Financial Promise</h1>
+            </div>
+            <div>
+                <ul>
+                    <li>Primeiro</li>
+                    <li>Segundo</li>
+                    <li>Terceiro</li>
+                </ul>
+            </div>
+            <div>
+                
+            </div>
+        </nav>
+    )
+}
