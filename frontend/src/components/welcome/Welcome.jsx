bb6577e901dbef13fb1b3ca1a67d31f163578266
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-10-22 08:08:06 -0300

    Criando breakpoints para responsividade e adicionando responsividade a Welcome

--- frontend/src/components/welcome/Welcome.jsx
+++ frontend/src/components/welcome/Welcome.jsx
@@ -1,9 +1,28 @@
 import { useTranslation } from "react-i18next";
-import moneyIncomeSVG from '../../assets/money-income-animate.svg'
+import { useMediaQuery } from 'react-responsive';
+import { breakpoints } from "../../styles/breakpoints";
+import moneyIncomeSVG from '../../assets/money-income-animate.svg';
 import './Welcome.css';
 
 function Welcome () {
-    const { t } = useTranslation();
+    const { t } = useTranslation(); //Using translations pt-br & en-us
+
+    //Using mediaQuery for detect breakpoints
+    
+    const isTablet = useMediaQuery(breakpoints.tablet);
+    const isMobile = useMediaQuery(breakpoints.mobile);
+
+    //Setting the text length according to breakpoint
+    let titleSize;
+    let textSize;
+
+    if(isTablet){
+        titleSize = '2rem';
+        textSize = '1.5rem';
+    } else if(isMobile) {
+        titleSize = '1.5rem';
+        textSize = '1rem';
+    }
 
     return(
         <>
@@ -13,8 +32,8 @@ function Welcome () {
                 </div>
 
                 <div className="welcome">
-                    <h1>{t('homeTitle')}</h1>
-                    <p>
+                    <h1 style={{fontSize: titleSize}}>{t('homeTitle')}</h1>
+                    <p style={{fontSize: textSize}}>
                         {t('homeText')}
                     </p>
 
---
+++ frontend/src/styles/breakpoints.jsx
@@ -0,0 +1,5 @@
+export const breakpoints = {
+    desktop: {minWidth: 1024},
+    tablet: {minWidth: 768, maxWidth: 1023},
+    mobile: {maxWidth: 767}
+}
